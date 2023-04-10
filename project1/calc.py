a4f86c28e99934b53da6a14b95b2fe807f147b0f
EC2 Default User <ec2-user@ip-172-31-83-67.ec2.internal>
2023-04-10 21:34:43 +0000

    don

--- project1/calc.py
+++ project1/calc.py
@@ -27,5 +27,6 @@ def result():
     entry = result
     return render_template('result.html', entry=entry)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port = 8080)
+    # app.run(debug=True)
